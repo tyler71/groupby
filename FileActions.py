ce9fe11fd1317a6f42d516fc7fd48526f15a1451
Tyler71 <tyler71@riseup.net>
2018-02-06 09:31:27 -0800

    Actions to complete on duplicate files

---
+++ FileActions.py
@@ -0,0 +1,27 @@
+import os
+
+def remove_files(filenames: iter) -> list:
+    removed_files = list()
+    for filename in filenames:
+        print(f"Removing {filename}")
+
+        try:
+            os.remove(filename)
+            removed_files.append(filename)
+        except FileNotFoundError:
+            print("Not Found")
+    return removed_files
+
+
+def hardlink_files(source_files: iter, duplicate_files: iter) -> list:
+    linked_files = list()
+    for source_file, filenames in zip(source_files, duplicate_files):
+        for filename in filenames:
+            print(f"Linking {source_file} -> {filename}")
+            try:
+                os.remove(filename)
+                os.link(source_file, filename)
+                linked_files.append((source_file, filename))
+            except FileNotFoundError:
+                print("Not Found")
+    return linked_files
