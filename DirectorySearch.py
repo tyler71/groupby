9ff531966f03fc96bf14597617766ee6b98ae13e
Tyler71 <tyler71@riseup.net>
2018-02-08 19:10:01 -0800

    Improved control of include, exclude
    Allowing usage of both.
    include overrides exclude

--- DirectorySearch.py
+++ DirectorySearch.py
@@ -2,23 +2,27 @@ import os
 import pathlib
 
 
-def recursive_directory_search(directory: str, include, exclude) -> tuple:
+def recursive_directory_search(directory: str, *, include, exclude) -> tuple:
     directory = os.path.expanduser(directory)
 
     for directory, subdir, files in os.walk(directory):
-        if exclude:
-            excluded_filenames = {file for glob_match in exclude
-                                  for file in files
-                                  if pathlib.PurePath(file).match(glob_match)}
-            for file in files:
-                if file not in excluded_filenames:
-                    yield os.path.join(directory, file)
-        elif include:
-            included_filenames = {file for glob_match in include
-                                  for file in files
-                                  if pathlib.PurePath(file).match(glob_match)}
+        if exclude or include:
+            if exclude:
+                excluded_filenames = {file for glob_match in exclude
+                                      for file in files
+                                      if pathlib.PurePath(file).match(glob_match)}
+            else:
+                excluded_filenames = set()
+            if include:
+                included_filenames = {file for glob_match in include
+                                      for file in files
+                                      if pathlib.PurePath(file).match(glob_match)}
+            else:
+                included_filenames = set()
             for file in files:
                 if file in included_filenames:
+                    yield os.path.join(directory, file)
+                elif file not in excluded_filenames and len(excluded_filenames) > 0:
                     yield os.path.join(directory, file)
         else:
             for file in files:
