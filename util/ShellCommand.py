2f80b8042057f0acd6f95ba9c04b11ba4d446c6c
Tyler71 <tyler71@riseup.net>
2018-02-25 21:15:29 -0800

    Working custom shell command
    Broken exec group command

--- util/ShellCommand.py
+++ util/ShellCommand.py
@@ -11,28 +11,29 @@ class ActionShell(argparse._AppendAction):
         items = _copy.copy(_ensure_value(namespace, self.dest, []))
         if isinstance(values, list):
             for template in values:
-                shell_command = convert_to_shell(invoke_shell, template)
+                template_format = convert_to_shell(template)
+                shell_command = partial(invoke_shell, command=template_format)
                 items.append(shell_command)
         else:
             template = values
-            shell_command = convert_to_shell(invoke_shell, template)
+            template_format = convert_to_shell(template)
+            shell_command = partial(invoke_shell, command=template_format)
             items.append(shell_command)
 
         setattr(namespace, self.dest, items)
 
-def convert_to_shell(func, template):
+def convert_to_shell(template):
     def wrapper(*args, **kwargs):
         template_func = template.format(*args, **kwargs)
-        shell_command = partial(func, command=template_func)
-        return shell_command
+        return template_func
     return wrapper
 
 
-def invoke_shell(filename: str, *, command,) -> str:
-    quoted_filename = shlex.quote(filename)
+def invoke_shell(*args, command, **kwargs) -> str:
+    args = [shlex.quote(arg) for arg in args]
     try:
-        output = subprocess.check_output(command(quoted_filename), shell=True).decode('utf8')
+        output = subprocess.check_output(command(*args, **kwargs), shell=True).decode('utf8')
     except subprocess.CalledProcessError as e:
         print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-        return b''
+        return ''
     return output.strip()
