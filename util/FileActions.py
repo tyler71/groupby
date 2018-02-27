7a23623c326bede9caa439f84bd6cc74b74f9fdf
Tyler71 <tyler71@riseup.net>
2018-02-27 08:33:22 -0800

    Increase availability of which files were linked

--- util/FileActions.py
+++ util/FileActions.py
@@ -14,11 +14,13 @@ def remove_files(filenames: iter) -> list:
 
 
 def hardlink_files(source_files: iter, duplicate_files: iter) -> list:
+    linked_files = list()
     for source_file, filename in zip(source_files, duplicate_files):
         print(f"Linking {source_file} -> {filename}")
         try:
             os.remove(filename)
             os.link(source_file, filename)
+            linked_files.append((source_file, filename))
         except FileNotFoundError:
             print("Not Found")
     return linked_files
