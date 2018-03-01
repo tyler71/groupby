d51804d3be6c44b151bd05e67c0beb8b299d22ba
Tyler71 <tyler71@riseup.net>
2018-02-28 18:24:50 -0800

    brace expansion

--- util/ShellCommand.py
+++ util/ShellCommand.py
@@ -2,7 +2,10 @@ import subprocess
 from functools import partial
 import argparse
 import shlex
+import string
+import os
 
+from pathlib import Path
 from argparse import _ensure_value, _copy
 
 
@@ -24,12 +27,50 @@ class ActionShell(argparse._AppendAction):
 
         setattr(namespace, self.dest, items)
 
+    def _process(self, template):
+        template = template.replace("{}", "{0}")
+        template = template.replace("{.}", "{:no_ext}")
+        template = template.replace("{/}", "{:basename}")
+        template = template.replace("{//}", "{:dir_path}")
+        template = template.replace("{/.}", "{:no_dir_ext}")
+        template = template.replace("{..}", "{:only_ext}")
+        template_format = Template(template)
+        shell_command = partial(invoke_shell, command=template_format)
+        return shell_command
 
-def format_template(template):
-    def wrapper(*args, **kwargs):
-        template_func = template.format(*args, **kwargs)
-        return template_func
-    return wrapper
+
+class TemplateFunc(string.Formatter):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+    def format_field(self, value, spec):
+        # {.} notation: Remove file extension
+        if spec.endswith("no_ext"):
+            split_ext = os.path.splitext(value)
+            value_no_ext = split_ext[0]
+            value = value_no_ext
+        # {/} notation: basename of list()file
+        if spec.endswith("basename"):
+            split_filename = os.path.split(value)[1]
+            value = split_filename
+        # {//} notation: directory of filename)
+        if spec.endswith("dirname"):
+            split_dir = os.path.split(value)[0]
+            value = split_dir
+        # {/.} notation: basename of file, with extentest removed
+        if spec == "no_ext_dir":
+            no_dir =  os.path.split(value)[1]
+            split_ext = os.path.splitext(no_dir)[0]
+            value = split_ext
+        # {..} notation: test extension of file
+        if spec.endswith("only_ext"):
+            ext = os.path.splitext(value)[1]
+            value = ext
+        return value, spec
 
 
 def invoke_shell(*args, command, **kwargs) -> str:
