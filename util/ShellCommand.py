60f711eed3d4a19e1165a25400d3ea9ad99396b7
Tyler71 <tyler71@riseup.net>
2018-02-28 18:33:10 -0800

    field spec can't be greater then 1 char

--- util/ShellCommand.py
+++ util/ShellCommand.py
@@ -28,46 +28,51 @@ class ActionShell(argparse._AppendAction):
         setattr(namespace, self.dest, items)
 
     def _process(self, template):
-        template = template.replace("{}", "{0}")
-        template = template.replace("{.}", "{:no_ext}")
-        template = template.replace("{/}", "{:basename}")
-        template = template.replace("{//}", "{:dir_path}")
-        template = template.replace("{/.}", "{:no_dir_ext}")
-        template = template.replace("{..}", "{:only_ext}")
         template_format = Template(template)
         shell_command = partial(invoke_shell, command=template_format)
         return shell_command
 
 
+def format_template(template):
+    def wrapper(*args, **kwargs):
+        template_func = template.format(*args, **kwargs)
+        return template_func
+    return wrapper
+
 class TemplateFunc(string.Formatter):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(self, template):
+        self.template = template
+        self.template = template.replace("{}", "{0}")
+        self.template = template.replace("{.}", "{:a}")
+        self.template = template.replace("{/}", "{:b}")
+        self.template = template.replace("{//}", "{:c}")
+        self.template = template.replace("{/.}", "{:e}")
+        self.template = template.replace("{..}", "{:f}")
 
     def __call__(self, *args, **kwargs):
         pass
 
     def format_field(self, value, spec):
         # {.} notation: Remove file extension
-        if spec.endswith("no_ext"):
+        if spec.endswith("a"):
             split_ext = os.path.splitext(value)
             value_no_ext = split_ext[0]
             value = value_no_ext
         # {/} notation: basename of list()file
-        if spec.endswith("basename"):
+        if spec.endswith("b"):
             split_filename = os.path.split(value)[1]
             value = split_filename
         # {//} notation: directory of filename)
-        if spec.endswith("dirname"):
+        if spec.endswith("c"):
             split_dir = os.path.split(value)[0]
             value = split_dir
         # {/.} notation: basename of file, with extentest removed
-        if spec == "no_ext_dir":
+        if spec == "e":
             no_dir =  os.path.split(value)[1]
             split_ext = os.path.splitext(no_dir)[0]
             value = split_ext
         # {..} notation: test extension of file
-        if spec.endswith("only_ext"):
+        if spec.endswith("f"):
             ext = os.path.splitext(value)[1]
             value = ext
         return value, spec
