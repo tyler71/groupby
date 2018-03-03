2b8fbcddd238875261c3c846dedcb98c7dcd8306
Tyler71 <tyler71@riseup.net>
2018-03-02 17:36:20 -0800

    Only complete regular expression on filename
    Not the relative / absolute path

--- util/ArgumentParsing.py
+++ util/ArgumentParsing.py
@@ -111,13 +111,15 @@ class ActionShell(ActionTemplate):
 
 class ActionRegex(ActionTemplate):
     def _process(self, template):
+        template = re.compile(template)
         regex_pattern = partial(self._re_match, pattern=template)
         return regex_pattern
 
-    def _re_match(self, filename, *, pattern):
-        filename = shlex.quote(filename)
-        pattern = re.compile(pattern)
-        result = pattern.search(filename)
+    def _re_match(self, filename, *, pattern) -> str:
+        split_file = os.path.split(filename)[1]
+        quoted_dir = shlex.quote(split_file)
+
+        result = pattern.search(quoted_dir)
         return result.group() if result else ""
 
 
